0a61e4192808cf74e1696068f13e10996b41edbd
Per Fahlander <perfah@kth.se>
2020-02-22 19:58:41 +0100

    Add comments to the BranchCoverage-class

--- src/main/java/com/esri/core/geometry/BranchCoverage.java
+++ src/main/java/com/esri/core/geometry/BranchCoverage.java
@@ -11,6 +11,22 @@ public class BranchCoverage {
     private int pos;
     private boolean returned;
 
+    private BranchCoverage(String funcName){
+        this.funcName = funcName;
+        coverage = new HashMap<>();
+        pos = 0;
+        returned = false;
+    }
+
+    // Get or create a new BranchCoverage-instance for the given function 'funcName':
+    public static BranchCoverage ofFunction(String funcName){
+        BranchCoverage bc = BranchCoverage.instances.getOrDefault(funcName, new BranchCoverage(funcName));
+        BranchCoverage.instances.putIfAbsent(bc.funcName, bc);
+        bc.reset();
+        return bc;
+    }
+
+    // Print the result of all reported BranchCoverage-instances when the process exits:
     public static void printOnExit(){
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             public void run() {
@@ -22,20 +38,7 @@ public class BranchCoverage {
         }, "Shutdown-thread"));
     }
 
-    public static BranchCoverage ofFunction(String funcName){
-        BranchCoverage bc = BranchCoverage.instances.getOrDefault(funcName, new BranchCoverage(funcName));
-        BranchCoverage.instances.putIfAbsent(bc.funcName, bc);
-        bc.reset();
-        return bc;
-    }
-
-    private BranchCoverage(String funcName){
-        this.funcName = funcName;
-        coverage = new HashMap<>();
-        pos = 0;
-        returned = false;
-    }
-
+    // Mark that the function is reentered (is automatically run with 'ofFunction' so you can ignore it):
     public void reset(){
         pos = 0;
     }
@@ -44,9 +47,8 @@ public class BranchCoverage {
         returned = true;
     }
 
-    // Gives a set of ID's that each point towards a branch corresponding to a condition (ifCondition or
-    // elseIfConditions or the else condition).
-    // parentBranchID: the
+    // Add a new branching point (that corresponds to one or many if/for-statements in the code)
+    // where program flow is differentially determined by the values of the parameters ifCondition and elseIfConditions:
     public void addBranchingPoint(boolean ifCondition, boolean... elseIfConditions){
         boolean pathAlreadyChoosen = ifCondition;
 
@@ -68,6 +70,7 @@ public class BranchCoverage {
         pos++;
     }
 
+    // Count the number of covered/visited branches:
     public int countVisitedBranches(){
         int numVisistedBranches = 0;
         for(boolean boolVal : coverage.values()){
@@ -77,10 +80,12 @@ public class BranchCoverage {
         return numVisistedBranches;
     }
 
+    // Count the number of possible branches to take in the code:
     public int countTotalBranches(){
         return coverage.size();
     }
 
+    // Get a ratio corresponding to the degree of branches covered:
     public double getBranchCoverageRatio(){
         return (double) countVisitedBranches() / (double) countTotalBranches();
     }
